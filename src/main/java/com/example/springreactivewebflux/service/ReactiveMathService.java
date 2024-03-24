320d63e74407da931c68d1601e278301889d1776
Shivakant6387 <shivakantsingh900@gmail.com>
2024-03-24 16:26:49 +0530

    using delayElements Duration ofsecond method

--- src/main/java/com/example/springreactivewebflux/service/ReactiveMathService.java
+++ src/main/java/com/example/springreactivewebflux/service/ReactiveMathService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
 @Service
 public class ReactiveMathService {
     public Mono<Response> findSquare(int input) {
@@ -14,7 +16,8 @@ public class ReactiveMathService {
 
     public Flux<Response> multiplicationTable(int input) {
         return Flux.range(1, 10)
-                .doOnNext(i -> SleepUtil.sleepSecond(1))
+//                .doOnNext(i -> SleepUtil.sleepSecond(1))
+                .delayElements(Duration.ofSeconds(1))
                 .doOnNext(i -> System.out.println("reactive-math-service processing :" + i))
                 .map(i -> new Response(i * input));
     }
