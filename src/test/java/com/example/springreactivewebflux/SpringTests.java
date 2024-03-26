87043d394e09b34fb29501de358ab2076aaf435e
Shivakant6387 <shivakantsingh900@gmail.com>
2024-03-26 15:15:15 +0530

    get mono refactor stepVerifier

--- src/test/java/com/example/springreactivewebflux/SpringTests.java
+++ src/test/java/com/example/springreactivewebflux/SpringTests.java
@@ -4,6 +4,8 @@ import com.example.springreactivewebflux.dto.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 class SpringTests extends BaseTest {
     @Autowired
@@ -18,5 +20,17 @@ class SpringTests extends BaseTest {
                 .bodyToMono(Response.class)
                 .block();
         System.out.println(response);
+    }
+
+    @Test
+    public void stepVerifierTest() {
+        Mono<Response> response = this.webClient
+                .get()
+                .uri("/api/reactive/math/square/{number}", 5)
+                .retrieve()
+                .bodyToMono(Response.class);
+        StepVerifier.create(response)
+                .expectNextMatches(r -> r.getOutput() == 25)
+                .verifyComplete();
     }
 }
